d41c95f98e57711d166a01ce90edd0648eb7eccc
Oh5Yeonju <oh5yeonju@gmail.com>
2023-09-19 15:52:39 +0900

    feat: #45 로그인 버튼 옆 로고 가상선택자로 변경

--- src/components/common/Button/LoginButton.jsx
+++ src/components/common/Button/LoginButton.jsx
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 const LoginButton = (props) => {
     return (
-        <LoginButtonStyle type="button">
-            <img key={props.props.id} src={props.props.logo} width="25" height="25" />
+        <LoginButtonStyle type="button" url={props.props.logo}>
+            {/*<img key={props.props.id} src={props.props.logo} width="25" height="25" />*/}
             {props.props.method}로 로그인
         </LoginButtonStyle>
     )
@@ -12,17 +12,25 @@ const LoginButton = (props) => {
 export default LoginButton;
 
 const LoginButtonStyle = styled.button`
-  display: flex;
+  position: relative;
   width: 100%;
   
   background-color: var(--basic-color);
   color: #fff;
   border-radius: 10px;
-  padding: 10px 0 10px 10px;
+  padding: 14px 0;
 
+  text-align: center;
   align-items: center;
   
-  img {
-    margin-right: 8px;
+  &:before {
+    content: "";
+    width: 25px;
+    height: 25px;
+    position: absolute;
+    top: 50%;
+    left: 20px;
+    transform: translateY(-50%);
+    background: url(${props => props.url});
   }
 `
